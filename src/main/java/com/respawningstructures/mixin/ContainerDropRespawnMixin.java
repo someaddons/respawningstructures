62f8fc1bb020c4acbca7d62bf6427911ae530540
someaddons <someaddons@github.com>
2024-02-17 12:14:16 +0100

    2.1: Add loot detection for containers being broken

--- src/main/java/com/respawningstructures/mixin/ContainerDropRespawnMixin.java
+++ src/main/java/com/respawningstructures/mixin/ContainerDropRespawnMixin.java
@@ -1,9 +1,14 @@
 package com.respawningstructures.mixin;
 
 import com.respawningstructures.structure.RespawnManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Container;
 import net.minecraft.world.Containers;
+import net.minecraft.world.entity.vehicle.ContainerEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,6 +29,38 @@ public class ContainerDropRespawnMixin
         if (RespawnManager.respawnInProgress != null)
         {
             ci.cancel();
+        }
+    }
+
+    /**
+     * Loot drop on destroyed
+     *
+     * @param level
+     * @param x
+     * @param y
+     * @param z
+     * @param container
+     * @param ci
+     */
+    @Inject(method = "dropContents(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/Container;)V", at = @At("HEAD"))
+    private static void onLootDropped(final Level level, final double x, final double y, final double z, final Container container, final CallbackInfo ci)
+    {
+        if (level instanceof ServerLevel)
+        {
+            if (container instanceof RandomizableContainerBlockEntity)
+            {
+                if (((RandomizableContainerBlockEntity) container).lootTable != null)
+                {
+                    RespawnManager.onChestLooted((ServerLevel) level, ((RandomizableContainerBlockEntity) container).lootTable, BlockPos.containing(x, y, z));
+                }
+            }
+            else if (container instanceof ContainerEntity)
+            {
+                if (((ContainerEntity) container).getLootTable() != null)
+                {
+                    RespawnManager.onChestLooted((ServerLevel) level, ((ContainerEntity) container).getLootTable(), BlockPos.containing(x, y, z));
+                }
+            }
         }
     }
 }
