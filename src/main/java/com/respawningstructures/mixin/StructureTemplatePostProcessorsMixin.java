c72b71a27564d0e256dfb8461183d73ca2e4ed53
someaddons <someaddons@github.com>
2025-07-10 21:43:08 +0200

    3.9: Config now supports structure tags Added blocktag "respawningstructures:keepexisting", which prevent overwriting a block on respawn if it still exists in the world.(by default portal and waystone blocks) Added blocktag "respawningstructures:norespawn", which prevents respawn placement of the given blocks

---
+++ src/main/java/com/respawningstructures/mixin/StructureTemplatePostProcessorsMixin.java
@@ -0,0 +1,45 @@
+package com.respawningstructures.mixin;
+
+import com.respawningstructures.event.EventHandler;
+import com.respawningstructures.structure.RespawnManager;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Mixin(StructureTemplate.class)
+public class StructureTemplatePostProcessorsMixin
+{
+    @Inject(method = "processBlockInfos(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;Ljava/util/List;Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructureTemplate;)Ljava/util/List;", at = @At(value = "RETURN"), remap = false)
+    private static void onReturn(
+        final ServerLevelAccessor serverLevelAccessor,
+        final BlockPos p_74519_,
+        final BlockPos p_74520_,
+        final StructurePlaceSettings p_74521_,
+        final List<StructureTemplate.StructureBlockInfo> p_74522_,
+        final StructureTemplate template,
+        final CallbackInfoReturnable<List<StructureTemplate.StructureBlockInfo>> cir)
+    {
+        if (RespawnManager.respawnInProgress != null)
+        {
+            List<StructureTemplate.StructureBlockInfo> list = cir.getReturnValue();
+            for (Iterator<StructureTemplate.StructureBlockInfo> iterator = list.iterator(); iterator.hasNext(); )
+            {
+                final StructureTemplate.StructureBlockInfo blockInfo = iterator.next();
+                if ((blockInfo.nbt() != null && blockInfo.state().is(EventHandler.KEEP_EXISTING)
+                    && serverLevelAccessor.getBlockState(blockInfo.pos()).getBlock().equals(blockInfo.state().getBlock()))
+                    || blockInfo.state().is(EventHandler.NO_RESPAWN))
+                {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+}
