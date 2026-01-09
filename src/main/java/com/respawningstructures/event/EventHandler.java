563678aee73af3c15762632a81d12abb3738461d
someaddons <someaddons@github.com>
2026-01-09 18:19:12 +0100

    4.0: Added new blocktag: "respawningstructures:keepalways" which prevents a respawning structure from overwriting that block. By default it contains grave blocks from various grave mods. Fixed the keep existing blocktag to no longer require waystones to be installed Fixed a rare crash, add more safety conditions Structure data now only counts Blockentity placement by real players

--- src/main/java/com/respawningstructures/event/EventHandler.java
+++ src/main/java/com/respawningstructures/event/EventHandler.java
@@ -33,6 +33,7 @@ public class EventHandler
     private final static TagKey<Block> REDSTONE = TagKey.create(Registries.BLOCK, new ResourceLocation(RespawningStructures.MOD_ID, "redstone"));
     public final static TagKey<Block> KEEP_EXISTING = TagKey.create(Registries.BLOCK, new ResourceLocation(RespawningStructures.MOD_ID, "keepexisting"));
     public final static TagKey<Block> NO_RESPAWN    = TagKey.create(Registries.BLOCK, new ResourceLocation(RespawningStructures.MOD_ID, "norespawn"));
+    public final static TagKey<Block> KEEP_ALWAYS = TagKey.create(Registries.BLOCK, new ResourceLocation(RespawningStructures.MOD_ID, "keepalways"));
     private static long lastTime = 0;
 
     @SubscribeEvent
