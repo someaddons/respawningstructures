46dfbddec42621f958a804f3e37dfbf883558576
someaddons <someaddons@github.com>
2026-05-02 14:04:04 +0200

    4.2: Fix crash with fake levels

--- src/main/java/com/respawningstructures/event/EventHandler.java
+++ src/main/java/com/respawningstructures/event/EventHandler.java
@@ -207,7 +207,9 @@ public class EventHandler
     @SubscribeEvent
     public static void onEntityAdded(final EntityJoinLevelEvent event)
     {
-        if (!event.getLevel().isClientSide && !RespawnManager.tryAddEntityDuringRespawn(event.getEntity(), (ServerLevel) event.getLevel(), event.getEntity().blockPosition()))
+        if (event.getLevel() instanceof ServerLevel && !RespawnManager.tryAddEntityDuringRespawn(event.getEntity(),
+            (ServerLevel) event.getLevel(),
+            event.getEntity().blockPosition()))
         {
             event.setCanceled(true);
         }
